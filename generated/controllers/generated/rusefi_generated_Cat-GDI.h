efea7efbe4f4c787db7156db1d44a2e825ad13fe
GitHub git update Action <action@github.com>
2024-07-15 23:54:08 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_Cat-GDI.h
+++ generated/controllers/generated/rusefi_generated_Cat-GDI.h
@@ -1700,7 +1700,7 @@
 #define ts_show_vvt_output true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2024.07.15.Cat-GDI.3799700599"
+#define TS_SIGNATURE "rusEFI main.2024.07.16.Cat-GDI.3799700599"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
