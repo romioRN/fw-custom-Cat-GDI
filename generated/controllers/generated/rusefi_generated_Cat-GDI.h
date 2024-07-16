6596abcca4e9da1047d981902b39187baafdd228
GitHub git update Action <action@github.com>
2024-07-16 23:02:30 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_Cat-GDI.h
+++ generated/controllers/generated/rusefi_generated_Cat-GDI.h
@@ -1288,7 +1288,7 @@
 #define show_microRusEFI_presets false
 #define show_Proteus_presets true
 #define show_test_presets false
-#define SIGNATURE_HASH 3799700599
+#define SIGNATURE_HASH 242280306
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -1700,7 +1700,7 @@
 #define ts_show_vvt_output true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2024.07.16.Cat-GDI.3799700599"
+#define TS_SIGNATURE "rusEFI main.2024.07.17.Cat-GDI.242280306"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
--- generated/controllers/generated/signature_Cat-GDI.h
+++ generated/controllers/generated/signature_Cat-GDI.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 3799700599
-#define TS_SIGNATURE "rusEFI main.2024.07.16.Cat-GDI.3799700599"
+#define SIGNATURE_HASH 242280306
+#define TS_SIGNATURE "rusEFI main.2024.07.17.Cat-GDI.242280306"
