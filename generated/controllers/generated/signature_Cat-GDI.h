cfc70a0d832e3eab7d11424c4e6c9f3f4810e627
GitHub git update Action <action@github.com>
2024-08-07 19:18:19 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_Cat-GDI.h
+++ generated/controllers/generated/signature_Cat-GDI.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 987892187
-#define TS_SIGNATURE "rusEFI main.2024.08.07.Cat-GDI.987892187"
+#define SIGNATURE_HASH 2938661793
+#define TS_SIGNATURE "rusEFI main.2024.08.07.Cat-GDI.2938661793"
